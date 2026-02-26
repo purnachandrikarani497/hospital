6013570c8a36c60933f2280d3a32678e66c45fe2
chandrika-1923 <purnachandrika@speshway.com>
2026-02-26 11:39:27 +0530

    update_api_to_use_env_vars

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,14 @@
-import axios from "axios";
-
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
-API.defaults.timeout = 8000;
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-export default API;
+import axios from "axios"; 
+  
+ const API = axios.create({ 
+   baseURL: process.env.REACT_APP_API_URL || "/api" 
+ }); 
+ API.defaults.timeout = 8000; 
+  
+ API.interceptors.request.use((config) => { 
+   const token = localStorage.getItem("token"); 
+   if (token) config.headers.Authorization = `Bearer ${token}`; 
+   return config; 
+ }); 
+  
+ export default API;
